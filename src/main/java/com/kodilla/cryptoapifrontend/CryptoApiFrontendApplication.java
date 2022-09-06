48c81d844abbc76edc24eb36177cd2da0ddd6760
Kreatorzy Wnętrz <KREATORZYWNETRZ@MacBook-Pro.local>
2022-09-06 13:21:50 +0200

    Initial commit

---
+++ src/main/java/com/kodilla/cryptoapifrontend/CryptoApiFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.cryptoapifrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApiFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoApiFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/cryptoapifrontend/CryptoApiFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.cryptoapifrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApiFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
